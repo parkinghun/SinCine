44bce74dd302076dfbc1dfe4e1fd45e722ef76b0
Sunghun Park <dfstem9@gmail.com>
2025-07-31 17:14:51 +0900

    프로젝트 생성

---
+++ SinCine/SinCine/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SinCine
+//
+//  Created by 박성훈 on 7/31/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
