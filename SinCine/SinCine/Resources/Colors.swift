689d9238b0b1f2c7aa10afcaf194d37915591f89
Sunghun Park <dfstem9@gmail.com>
2025-07-31 22:37:48 +0900

    chore: 프로젝트 초기 세팅
    - 앱 전체적으로 사용할 Colors 추가
    - 앱에서 사용할 이미지 추가
    - 스플래시 이미지 에셋에 추가
    - 스토리보드 삭제
    - SPM 추가
    
    Github issue #2

---
+++ SinCine/SinCine/Resources/Colors.swift
@@ -0,0 +1,16 @@
+//
+//  Colors.swift
+//  SinCine
+//
+//  Created by 박성훈 on 7/31/25.
+//
+
+import UIKit
+
+enum Colors {
+    static let mainColor = UIColor(hexCode: "98FB98")
+    static let lightGray = UIColor(hexCode: "E1E1E1")
+    static let darkGray = UIColor(hexCode: "8E8E8E")
+    static let black = UIColor(hexCode: "000000")
+    static let white = UIColor(hexCode: "FFFFFF")
+}
---
+++ SinCine/SinCine/Resources/Images.swift
@@ -0,0 +1,20 @@
+//
+//  Images.swift
+//  SinCine
+//
+//  Created by 박성훈 on 7/31/25.
+//
+
+import UIKit
+
+enum Images {
+    static let heart = UIImage(systemName: "heart")
+    static let heartFill = UIImage(systemName: "heart.fill")
+    static let srarFill = UIImage(systemName: "star.fill")
+    static let filmFill = UIImage(systemName: "film.fill")
+    static let cameraFill = UIImage(systemName: "camera.fill")
+    static let magnifyingglass = UIImage(systemName: "magnifyingglass")
+    static let xmark = UIImage(systemName: "xmark")
+    static let calendar = UIImage(systemName: "calendar")
+
+}
